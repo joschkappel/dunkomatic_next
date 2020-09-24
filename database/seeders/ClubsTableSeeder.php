c411d866978db96d23a1f1eda759cb817130d205
jochen kappel <jkappel@onlinehome.de>
2020-09-24 18:20:42 +0200

    test

---
+++ database/seeders/ClubsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ClubsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      $old_club = DB::connection('dunkv1')->table('club')->get();
+
+      foreach ($old_club as $club) {
+        DB::connection('dunknxt')->table('clubs')->insert([
+          'shortname'     => $club->shortname,
+          'region'        => $club->region,
+          'name'          => $club->name,
+          'club_no'       => $club->club_no,
+          'url'           => $club->club_url,
+          'created_at'    => now(),
+          'id'            => $club->club_id,
+        ]);
+
+      }
+    }
+}
