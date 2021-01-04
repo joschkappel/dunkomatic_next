70cb6bf78de52d1a8ce109192e85d8d42aeb4abf
jochen kappel <jkappel@onlinehome.de>
2021-01-04 10:00:48 +0100

    latest aws codedeploy

--- .platform/hooks/postdeploy/01_config_laravel.sh
+++ .platform/hooks/postdeploy/01_config_laravel.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 chown -R ec2-user /var/app/current/
 # cd /var/app/current/
-cp .env.aws .env
+mv .aws .env
 chmod -R 0777 storage
 chmod 777 storage/framework/cache
--- .platform/hooks/prebuild/01_setenv.sh
+++ .platform/hooks/prebuild/01_setenv.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 cd /var/app/staging/
-sudo cp .env.aws .env
+sudo cp .aws .env
 sudo chmod -R 0777 storage
 sudo chmod 777 storage/framework/cache
