26448a14ef98d7fc480fb8a19c621f808ad0d7b3
jochen kappel <jkappel@onlinehome.de>
2021-10-06 14:20:34 +0200

    dusk test fix select2

--- tests/Browser/Pages/League/NewLeague.php
+++ tests/Browser/Pages/League/NewLeague.php
@@ -41,7 +41,9 @@ class NewLeague extends Page
     }
 
     public function create_league( Browser $browser, $code, $name ){
-      $browser->select2('.js-selSize', '4 Teams')
+      $browser->select2('.js-selSize', '4 Tea')
+              ->assertSeeIn('.js-selSize + .select2', '4 Teams')
+              ->click('h3:first-child') // close the previous select
               ->screenshot('Size selected')
               ->type('shortname',$code)
               ->type('name',$name)
