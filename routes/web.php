3fc0ae425ef115f199bd26f2af677ead77a7504c
jochen kappel <jkappel@onlinehome.de>
2021-11-08 16:45:09 +0100

    Switching regions on non-region views Fixes #189

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::get('/health', function () {
 Route::group([
     'prefix' => '{language}',
     'where' => ['language' => '[a-zA-Z]{2}'],
-    'middleware' => 'set.language'
+    'middleware' => ['set.language','set.region']
 ], function () {
 
     Route::get('/', function () {
@@ -69,7 +69,7 @@ Route::group([
         Route::get('club/{club}/game/chart', 'ClubGameController@chart')->name('club.game.chart')->middleware('can:view-games');;
         Route::get('club/{club}/team/pickchar', 'ClubTeamController@pickchar')->name('club.team.pickchar')->middleware('can:update-teams');;
 
-        Route::group(['prefix' => 'region/{region}', 'middleware' => 'set.region'], function () {
+        Route::group(['prefix' => 'region/{region}'], function () {
             Route::get('club', 'ClubController@index')->name('club.index')->middleware('can:view-clubs');
             Route::get('club/create', 'ClubController@create')->name('club.create')->middleware('can:create-clubs');
             Route::get('league/list', 'LeagueController@list')->name('league.list')->middleware('can:view-leagues');
@@ -99,7 +99,7 @@ Route::group([
         Route::resource('club.gym', 'ClubGymController')->shallow()->except('store', 'update', 'destroy', 'show');
 
 
-        Route::get('league/{league}/dashboard', 'LeagueController@dashboard')->name('league.dashboard')->middleware('set.region');
+        Route::get('league/{league}/dashboard', 'LeagueController@dashboard')->name('league.dashboard');
         Route::get('league/{league}/briefing', 'LeagueController@briefing')->name('league.briefing')->middleware('can:view-leagues');
         Route::get('league/{league}', 'LeagueController@show')->name('league.show')->middleware('can:view-leagues');
         Route::get('league/{league}/edit', 'LeagueController@edit')->name('league.edit')->middleware('can:update-leagues');
@@ -140,7 +140,7 @@ Route::group([
 
 Route::get('region/admin/sb', 'RegionController@admin_sb')->name('region.admin.sb');
 
-Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth','set.region'])->group(function () {
     // APIs , no locale or language required !
     Route::redirect('home', '/de/home');
 
@@ -171,7 +171,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('team/{team}/list/gym', 'ClubGymController@sb_team')->name('gym.sb.team');
     Route::resource('club.gym', 'ClubGymController')->shallow()->only('store', 'update', 'destroy');
 
-    Route::group(['prefix' => 'region/{region}', 'middleware' => 'set.region'], function () {
+    Route::group(['prefix' => 'region/{region}'], function () {
         Route::get('club/list', 'ClubController@list')->name('club.list')->middleware('can:view-clubs');
         Route::post('club', 'ClubController@store')->name('club.store')->middleware('can:create-clubs');
         Route::post('league', 'LeagueController@store')->name('league.store')->middleware('can:create-leagues');
