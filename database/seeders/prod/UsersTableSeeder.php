0eca9eb968d29ad72252ba5c13139465a34f1eca
jochen kappel <jkappel@onlinehome.de>
2022-02-22 09:11:50 +0100

    fix user seeders

--- database/seeders/prod/UsersTableSeeder.php
+++ database/seeders/prod/UsersTableSeeder.php
@@ -26,7 +26,9 @@ class UsersTableSeeder extends Seeder
           'approved_at' => now(),
           'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ]);
-        Bouncer::assign('superadmin')->to(User::find($uid));
+        $u = User::find($uid);
+        Bouncer::assign('superadmin')->to($u);
+        Bouncer::allow( $u )->to('manage', $u);
 
 
     }
