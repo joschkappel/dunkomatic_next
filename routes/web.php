10eeb8c8a36293db68d5658e7f576dfc0f5c69e1
jochen kappel <jkappel@onlinehome.de>
2022-02-24 12:35:07 +0100

    fixing routes

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,7 @@ use App\Http\Controllers\LeagueSizeSchemeController;
 use App\Http\Controllers\FileDownloadController;
 use App\Http\Controllers\LeagueGameController;
 use App\Http\Controllers\LeagueController;
+use App\Http\Controllers\LeagueStateController;
 
 /*
 |--------------------------------------------------------------------------
