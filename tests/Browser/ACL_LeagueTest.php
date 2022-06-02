9fffa6305dbcd36e1dd7a2ebf911c9a5bdedfe85
jochen kappel <jkappel@onlinehome.de>
2022-06-02 13:03:46 +0200

    dusk test fix

--- tests/Browser/ACL_LeagueTest.php
+++ tests/Browser/ACL_LeagueTest.php
@@ -203,7 +203,7 @@ class ACL_LeagueTest extends DuskTestCase
         $this->browse(function ($browser) use ($user, $league) {
             $browser->loginAs($user)->visitRoute('league.index_mgmt',['language'=>'de','region'=>static::$region]);
 
-            if ( $user->canAny(['create-leagues','update-leagues']) ) {
+            if ( $user->can('access', $league) ) {
                 $browser->assertRouteIs('league.index_mgmt',['language'=>'de','region'=>static::$region]);
                 ($user->can('create-leagues')) ? $browser->assertSee(__('league.action.create',$locale=['de'])) : $browser->assertDontSee(__('league.action.create',$locale=['de']));
                 $browser->waitFor('.table')->assertSeeLink($league->shortname)->clickLink($league->shortname)->assertRouteIs('league.dashboard', ['language'=>'de','league'=>$league->id]);
