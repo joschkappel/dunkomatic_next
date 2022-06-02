20dafdf414d35764b5b0cb222d20b07af32b7070
jochen kappel <jkappel@onlinehome.de>
2022-06-02 13:42:34 +0200

    dusk test mod

--- tests/Browser/ACL_LeagueTest.php
+++ tests/Browser/ACL_LeagueTest.php
@@ -205,7 +205,7 @@ class ACL_LeagueTest extends DuskTestCase
 
             $browser->assertRouteIs('league.index_mgmt',['language'=>'de','region'=>static::$region]);
             ($user->can('create-leagues')) ? $browser->assertSee(__('league.action.create',$locale=['de'])) : $browser->assertDontSee(__('league.action.create',$locale=['de']));
-            $browser->waitFor('.table', 5)->assertSeeLink($league->shortname)->clickLink($league->shortname);
+            $browser->waitFor('.table', 10)->assertSeeLink($league->shortname)->clickLink($league->shortname);
             ( $user->can('access', $league) )  ? $browser->assertRouteIs('league.dashboard', ['language'=>'de','league'=>$league->id]) : $browser->assertRouteIs('league.briefing', ['language'=>'de','league'=>$league->id]);
         });
     }
