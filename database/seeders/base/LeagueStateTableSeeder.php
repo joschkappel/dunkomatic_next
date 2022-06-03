156940112d88d493e6bbb98e4acd09671a2e8f78
jochen kappel <jkappel@onlinehome.de>
2022-06-03 12:19:16 +0200

    tests fixed

--- database/seeders/base/LeagueStateTableSeeder.php
+++ database/seeders/base/LeagueStateTableSeeder.php
@@ -22,8 +22,7 @@ class LeagueStateTableSeeder extends Seeder
         foreach ($leagues as $l){
             if ( ($l->schedule()->exists() ) and ($l->league_size()->exists()) ){
                 $kpis = $l->state_count;
-                if ( $kpis['assigned'] > 0 ) { $this->reopen_club_assignment($l);$l->refresh();}
-                if ( $kpis['registered'] > 0 ) { $this->open_team_registration($l);$l->refresh();}
+                if ( $kpis['registered'] > 0 ) { $this->start_league($l);$l->refresh();}
                 if ( $kpis['charspicked'] > 0 ) { $this->open_char_selection($l);$l->refresh();}
                 if ( $kpis['generated'] > 0 ) { $this->open_game_scheduling($l);$l->refresh();}
                 if ( ($kpis['generated']>0) and ($kpis['scheduled'] == $kpis['generated']) ) { $this->open_ref_assignment($l);$l->refresh();}
