499135280377ef0c7df6d3d102f79c318ee753c7
jochen kappel <jkappel@onlinehome.de>
2022-06-08 18:59:26 +0200

    some i18n translations

---
+++ lang/de/passwords.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+  'sent' => 'Die e-Mail zum Zurücksetzen wurde versendet',
+  'throttled' => 'Das sind zuviele Versuche. Die e-Mail wurde ncht versendet. Versuche es später nochmal.',
+];
---
+++ lang/en/passwords.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+  'sent' => 'Password reset e-Mail has been sent.',
+  'throttled' => 'Too many tries. Please tra again later.',
+];
