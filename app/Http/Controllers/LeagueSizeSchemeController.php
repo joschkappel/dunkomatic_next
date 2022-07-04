971458c17281fd4971e747862d66b4ed1fd5fef3
jochen kappel <jkappel@onlinehome.de>
2022-07-04 15:11:08 +0200

    Add table for league number matching Fixes #463

--- app/Http/Controllers/LeagueSizeSchemeController.php
+++ app/Http/Controllers/LeagueSizeSchemeController.php
@@ -63,22 +63,136 @@ class LeagueSizeSchemeController extends Controller
         $schemelist = datatables()::of($scheme);
 
         return $schemelist
-            ->editColumn('1', function ($d) { return $d->{1} == ' ' ? '' : '1 - '.$d->{1};  })
-            ->editColumn('2', function ($d) { return $d->{2} == ' ' ? '' : '2 - '.$d->{2};  })
-            ->editColumn('3', function ($d) { return $d->{3} == ' ' ? '' : '3 - '.$d->{3};  })
-            ->editColumn('4', function ($d) { return $d->{4} == ' ' ? '' : '4 - '.$d->{4};  })
-            ->editColumn('5', function ($d) { return $d->{5} == ' ' ? '' : '5 - '.$d->{5};  })
-            ->editColumn('6', function ($d) { return $d->{6} == ' ' ? '' : '6 - '.$d->{6};  })
-            ->editColumn('7', function ($d) { return $d->{7} == ' ' ? '' : '7 - '.$d->{7};  })
-            ->editColumn('8', function ($d) { return $d->{8} == ' ' ? '' : '8 - '.$d->{8};  })
-            ->editColumn('9', function ($d) { return $d->{9} == ' ' ? '' : '9 - '.$d->{9};  })
-            ->editColumn('10', function ($d) { return $d->{10} == ' ' ? '' : '10 - '.$d->{10};  })
-            ->editColumn('11', function ($d) { return $d->{11} == ' ' ? '' : '11 - '.$d->{11};  })
-            ->editColumn('12', function ($d) { return $d->{12} == ' ' ? '' : '12 - '.$d->{12};  })
-            ->editColumn('13', function ($d) { return $d->{13} == ' ' ? '' : '13 - '.$d->{13};  })
-            ->editColumn('14', function ($d) { return $d->{14} == ' ' ? '' : '14 - '.$d->{14};  })
-            ->editColumn('15', function ($d) { return $d->{15} == ' ' ? '' : '15 - '.$d->{15};  })
-            ->editColumn('16', function ($d) { return $d->{16} == ' ' ? '' : '16 - '.$d->{16};  })
+            ->rawColumns(['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16'])
+            ->editColumn('1', function ($d) { return $d->{1} == ' ' ? '' : '<span class="badge text-sm badge-pill badge-secondary">1 - '.$d->{1}.'</span>';  })
+            ->editColumn('2', function ($d) { return $d->{2} == ' ' ? '' : '<span class="badge text-sm badge-pill badge-primary">2 - '.$d->{2}.'</span>';  })
+            ->editColumn('3', function ($d) { return $d->{3} == ' ' ? '' : '<span class="badge text-sm badge-pill badge-secondary">3 - '.$d->{3}.'</span>';  })
+            ->editColumn('4', function ($d) { return $d->{4} == ' ' ? '' : '<span class="badge text-sm badge-pill badge-primary">4 - '.$d->{4}.'</span>';  })
+            ->editColumn('5', function ($d) { return $d->{5} == ' ' ? '' : '<span class="badge text-sm badge-pill badge-secondary">5 - '.$d->{5}.'</span>';  })
+            ->editColumn('6', function ($d) { return $d->{6} == ' ' ? '' : '<span class="badge text-sm badge-pill badge-primary">6 - '.$d->{6}.'</span>';  })
+            ->editColumn('7', function ($d) { return $d->{7} == ' ' ? '' : '<span class="badge text-sm badge-pill badge-secondary">7 - '.$d->{7}.'</span>';  })
+            ->editColumn('8', function ($d) { return $d->{8} == ' ' ? '' : '<span class="badge text-sm badge-pill badge-primary">8 - '.$d->{8}.'</span>';  })
+            ->editColumn('9', function ($d) { return $d->{9} == ' ' ? '' : '<span class="badge text-sm badge-pill badge-secondary">9 - '.$d->{9}.'</span>';  })
+            ->editColumn('10', function ($d) { return $d->{10} == ' ' ? '' : '<span class="badge text-sm badge-pill badge-secondary">10 - '.$d->{10}.'</span>';  })
+            ->editColumn('11', function ($d) { return $d->{11} == ' ' ? '' : '<span class="badge text-sm badge-pill badge-primary">11 - '.$d->{11}.'</span>';  })
+            ->editColumn('12', function ($d) { return $d->{12} == ' ' ? '' : '<span class="badge text-sm badge-pill badge-secondary">12 - '.$d->{12}.'</span>';  })
+            ->editColumn('13', function ($d) { return $d->{13} == ' ' ? '' : '<span class="badge text-sm badge-pill badge-primary">13 - '.$d->{13}.'</span>';  })
+            ->editColumn('14', function ($d) { return $d->{14} == ' ' ? '' : '<span class="badge text-sm badge-pill badge-secondary">14 - '.$d->{14}.'</span>';  })
+            ->editColumn('15', function ($d) { return $d->{15} == ' ' ? '' : '<span class="badge text-sm badge-pill badge-primary">15 - '.$d->{15}.'</span>';  })
+            ->editColumn('16', function ($d) { return $d->{16} == ' ' ? '' : '<span class="badge text-sm badge-pill badge-secondary">16 - '.$d->{16}.'</span>';  })
             ->make(true);
+    }
+
+    /**
+     * Display a matrix showing the home game matches between team league numbers
+     *
+     * @param \App\Models\LeagueSize $size
+     * @return \Illuminate\Http\JsonResponse
+     *
+     */
+    public function list_match(LeagueSize $size){
+        Log::info('preparing laegue scheme match table.', ['league-size-id'=>$size->id]);
+
+        $scheme = $size->schemes;
+        $league_no_a = $scheme->groupBy('team_home')->sortKeys();
+        $league_no_b = $scheme->groupBy('team_home')->sortKeys();
+
+        $matches = collect();
+        $max_days = ($size->size - 1) ;
+
+        foreach($league_no_a as $no_a){
+            $row = array('league_no'=> $no_a->first()->team_home);
+            foreach($league_no_b as $no_b){
+                // only do have matrix
+                if ( $no_b->first()->team_home >= $no_a->first()->team_home ){
+                    $common_homegames = $no_a->pluck('game_day')->intersect( $no_b->pluck('game_day'))->count();
+                    if (  $no_b->first()->team_home == $no_a->first()->team_home ){
+                        $cell = '<span class="badge text-sm badge-pill badge-danger text-white">';
+                    } else {
+                        if (  $common_homegames >= $max_days*0.8 ){
+                            $cell = '<span class="badge text-sm badge-pill text-white" style="background-color:SeaGreen">';
+                        } elseif (  $common_homegames >= $max_days*0.6 ){
+                            $cell = '<span class="badge text-sm badge-pill text-white" style="background-color:LimeGreen">';
+                        } elseif (  $common_homegames >= $max_days*0.6 ){
+                            $cell = '<span class="badge text-sm badge-pill " style="background-color:GreenYellow">';
+                        } else {
+                            $cell = '<span>';
+                        }
+                    }
+
+                    $cell .= ($common_homegames*2).'</span>';
+                    $row += [$no_b->first()->team_home => $cell  ];
+                } else {
+                    $row += [$no_b->first()->team_home => '' ];
+                }
+            }
+            // populate missing table cols
+            foreach(range($size->size+1, 16) as $i){
+                $row += [$i=>''];
+            }
+
+            $matches->push($row);
+        }
+
+        $matchlist = datatables($matches);
+        Log::info('Scheme matches calculated');
+
+        return $matchlist
+            ->rawColumns(['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16'])
+            ->make(true);
+
+    }
+
+    /**
+     * Display a matrix showing the home game matches between 2 schemes
+     * @param \App\Models\LeagueSize $size
+     * @return \Illuminate\Http\JsonResponse
+     *
+     */
+    public function list_compare(LeagueSize $size1, LeagueSize $size2){
+        Log::info('preparing laegue scheme comparison table.', ['league-size-id1'=>$size1->id, 'league-size-id2'=>$size2->id]);
+
+        $scheme1 = $size1->schemes;
+        $league_no_a = $scheme1->groupBy('team_home')->sortKeys();
+        $scheme2 = $size2->schemes;
+        $league_no_b = $scheme2->groupBy('team_home')->sortKeys();
+
+        $matches = collect();
+        $max_days = min(($size1->size - 1),($size2->size - 1)) ;
+
+        foreach($league_no_a as $no_a){
+            $row = array('league_no'=> $no_a->first()->team_home);
+            foreach($league_no_b as $no_b){
+                $common_homegames = $no_a->pluck('game_day')->intersect( $no_b->pluck('game_day'))->count();
+                if (  $common_homegames == $max_days ){
+                    $cell = '<span class="badge text-sm badge-pill text-white badge-danger">';
+                } elseif (  $common_homegames >= $max_days*0.8 ){
+                    $cell = '<span class="badge text-sm badge-pill text-white" style="background-color:SeaGreen">';
+                } elseif (  $common_homegames >= $max_days*0.6 ){
+                    $cell = '<span class="badge text-sm badge-pill text-white" style="background-color:LimeGreen">';
+                } elseif (  $common_homegames >= $max_days*0.6 ){
+                    $cell = '<span class="badge text-sm badge-pill " style="background-color:GreenYellow">';
+                } else {
+                    $cell = '<span>';
+                }
+
+                $cell .= ($common_homegames*2).'</span>';
+                $row += [$no_b->first()->team_home => $cell  ];
+            }
+            // populate missing table cols
+            foreach(range($size2->size+1, 16) as $i){
+                $row += [$i=>''];
+            }
+
+            $matches->push($row);
+        }
+
+        $matchlist = datatables($matches);
+        Log::info('Scheme comparison calculated');
+
+        return $matchlist
+            ->rawColumns(['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16'])
+            ->make(true);
+
     }
 }
--- resources/views/league/league_scheme_list.blade.php
+++ resources/views/league/league_scheme_list.blade.php
@@ -1,12 +1,20 @@
 @extends('layouts.page')
 
 @section('content_header')
-<div class="row">
-    <div class="col-8">
+<div class="row d-flex justify-content-between">
+    <div class="col-4">
         <div>
             <label for='selSize'>@lang('schedule.action.size.select')</label>
-            <select class='js-example-placeholder-single js-states form-control select2'
+            <select class='js-example-placeholder-single js-size1 js-states form-control select2'
                 id='selSize'>
+            </select>
+        </div>
+    </div>
+    <div class="col-4">
+        <div>
+            <label for='selSize2'>@lang('schedule.action.size.compare')</label>
+            <select class='js-example-placeholder-single js-size2 js-states form-control select2'
+                id='selSize2'>
             </select>
         </div>
     </div>
@@ -14,7 +22,7 @@
 @stop
 
 @section('content')
-<x-card-list cardTitle="{{ trans_choice('schedule.scheme',1) }}" >
+<x-card-list cardTitle="{{ __('schedule.title.scheme.games',['size'=>'?']) }}" >
     <th>@lang('game.game_day')</th>
     <th>@lang('game.team_home'): 1</th>
     <th>2</th>
@@ -38,6 +46,52 @@
 </x-card-list>
 
 @include('league.includes.league_scheme_list_help')
+
+<x-card-list tableId="tblSchemeMatch"  cardTitle="{{ __('schedule.title.scheme.matches', ['size'=>'?']) }}">
+    <th>Ziffern</th>
+    <th>1</th>
+    <th>2</th>
+    <th>3</th>
+    <th>4</th>
+    <th>5</th>
+    <th>6</th>
+    <th>7</th>
+    <th>8</th>
+    <th>9</th>
+    <th>10</th>
+    <th>11</th>
+    <th>12</th>
+    <th>13</th>
+    <th>14</th>
+    <th>15</th>
+    <th>16</th>
+    <x-slot:addButtons>
+        <button type="button" class="btn btn-outline-secondary mr-2" id="getHelp">{{ __('Help')}}</button>
+    </x-slot:addButtons>
+</x-card-list>
+
+<x-card-list tableId="tblSchemeCompare"  cardTitle="{{ __('schedule.title.scheme.xmatches', ['size1'=>'?', 'size2'=>'?']) }}">
+    <th>Ziffern</th>
+    <th>1</th>
+    <th>2</th>
+    <th>3</th>
+    <th>4</th>
+    <th>5</th>
+    <th>6</th>
+    <th>7</th>
+    <th>8</th>
+    <th>9</th>
+    <th>10</th>
+    <th>11</th>
+    <th>12</th>
+    <th>13</th>
+    <th>14</th>
+    <th>15</th>
+    <th>16</th>
+    <x-slot:addButtons>
+        <button type="button" class="btn btn-outline-secondary mr-2" id="getHelp">{{ __('Help')}}</button>
+    </x-slot:addButtons>
+</x-card-list>
 @stop
 
 @section('js')
@@ -71,6 +125,8 @@
             var myTable = $('#table').DataTable({
                 responsive: true,
                 paging: false,
+                bSort: false,
+                bFilter: false,
                 language: { "url": "{{URL::asset('lang/vendor/datatables.net/'.app()->getLocale().'.json')}}" },
                 columns: [
                     { data: 'game_day', name: 'game_day' },
@@ -92,12 +148,88 @@
                     { data: '16', name: '16' }
                 ]
             });
+            var myMatchTable = $('#tblSchemeMatch').DataTable({
+                responsive: true,
+                paging: false,
+                bSort: false,
+                bFilter: false,
+                language: { "url": "{{URL::asset('lang/vendor/datatables.net/'.app()->getLocale().'.json')}}" },
+                columns: [
+                    { data: 'league_no', name: 'league_no' },
+                    { data: '1', name: '1' },
+                    { data: '2', name: '2' },
+                    { data: '3', name: '3' },
+                    { data: '4', name: '4' },
+                    { data: '5', name: '5' },
+                    { data: '6', name: '6' },
+                    { data: '7', name: '7' },
+                    { data: '8', name: '8' },
+                    { data: '9', name: '9' },
+                    { data: '10', name: '10' },
+                    { data: '11', name: '11' },
+                    { data: '12', name: '12' },
+                    { data: '13', name: '13' },
+                    { data: '14', name: '14' },
+                    { data: '15', name: '15' },
+                    { data: '16', name: '16' }
+                ]
+            });
+            var myCompareTable = $('#tblSchemeCompare').DataTable({
+                responsive: true,
+                paging: false,
+                bSort: false,
+                bFilter: false,
+                language: { "url": "{{URL::asset('lang/vendor/datatables.net/'.app()->getLocale().'.json')}}" },
+                columns: [
+                    { data: 'league_no', name: 'league_no' },
+                    { data: '1', name: '1' },
+                    { data: '2', name: '2' },
+                    { data: '3', name: '3' },
+                    { data: '4', name: '4' },
+                    { data: '5', name: '5' },
+                    { data: '6', name: '6' },
+                    { data: '7', name: '7' },
+                    { data: '8', name: '8' },
+                    { data: '9', name: '9' },
+                    { data: '10', name: '10' },
+                    { data: '11', name: '11' },
+                    { data: '12', name: '12' },
+                    { data: '13', name: '13' },
+                    { data: '14', name: '14' },
+                    { data: '15', name: '15' },
+                    { data: '16', name: '16' }
+                ]
+            });
 
-            $(".js-example-placeholder-single").on('select2:select', function(e) {
+            $(".js-size1").on('select2:select', function(e) {
                 var data = e.params.data;
                 var url = '{{ route('scheme.list_piv', ['size' => ':size:']) }}';
                 url = url.replace(':size:', data.id);
                 myTable.ajax.url( url ).load();
+                var title1 = '{{__('schedule.title.scheme.games',['size'=>':size:'])}}';
+                var title2 = '{{__('schedule.title.scheme.matches',['size'=>':size:'])}}';
+                title1 = title1.replace(':size:', '"'+data.text+'"');
+                title2 = title2.replace(':size:', '"'+data.text+'"');
+                $('#titletable').html(title1);
+                $('#titletblSchemeMatch').html(title2);
+
+                var url2 = '{{ route('scheme.list_match', ['size' => ':size:']) }}';
+                url2 = url2.replace(':size:', data.id);
+                myMatchTable.ajax.url( url2 ).load();
+
+            });
+            $(".js-size2").on('select2:select', function(e) {
+                var data1 = $('.js-size1').select2('data');
+                var data2 = e.params.data;
+                var title3 = '{{__('schedule.title.scheme.xmatches',['size1'=>':size1:','size2'=>':size2:'])}}';
+                title3 = title3.replace(':size1:', '"'+data1[0].text+'"');
+                title3 = title3.replace(':size2:', '"'+data2.text+'"');
+                $('#titletblSchemeCompare').html(title3);
+
+                var url3 = '{{ route('scheme.list_compare', ['size1' => ':size1:', 'size2' => ':size2:']) }}';
+                url3 = url3.replace(':size2:', data2.id);
+                url3 = url3.replace(':size1:', data1[0].id);
+                myCompareTable.ajax.url( url3 ).load();
 
             });
         });
