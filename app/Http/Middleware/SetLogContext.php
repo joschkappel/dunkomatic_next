10037d34b404d39419988419d1a6fd760f1d31a8
jochen kappel <jkappel@onlinehome.de>
2022-07-05 15:11:53 +0200

    add route name to logs

--- app/Http/Middleware/SetLogContext.php
+++ app/Http/Middleware/SetLogContext.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Auth;
@@ -23,6 +24,7 @@ class SetLogContext
 
         Log::withContext([
             // 'request-id' => $requestId,
+            'route-name' => Route::currentRouteName(),
             'user-id' => Auth::user()->id ?? 'not logged in',
             'region-id' => session('cur_region')->id ?? 'no region set'
         ]);
