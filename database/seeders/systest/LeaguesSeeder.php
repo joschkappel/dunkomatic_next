d68cc4f2393290576af8f9ecef057c1142019dd7
jochen kappel <jkappel@onlinehome.de>
2022-09-02 16:38:25 +0200

    systest data fixed

--- database/seeders/systest/LeaguesSeeder.php
+++ database/seeders/systest/LeaguesSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders\systest;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\League;
 
 class LeaguesSeeder extends Seeder
 {
@@ -18,5 +19,14 @@ class LeaguesSeeder extends Seeder
         DB::unprepared(file_get_contents( __dir__ . '/sql/club_league.sql'));
         DB::unprepared(file_get_contents( __dir__ . '/sql/teams.sql'));
         DB::unprepared(file_get_contents( __dir__ . '/sql/games.sql'));
+
+        // createa 20 leagues
+/*         for ($i=0; $i < 10; $i++) {
+            League::factory()->registered(8)->create(['league_size_id'=>5]);
+            League::factory()->selected(10, $i)->create(['league_size_id'=>5]);
+            League::factory()->frozen(10, $i)->create(['league_size_id'=>5]);
+            League::factory()->registered(10, $i)->create(['league_size_id'=>5]);
+        } */
+
     }
 }
--- database/seeders/systest/SchedulesSeeder.php
+++ database/seeders/systest/SchedulesSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders\systest;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\Schedule;
 
 class SchedulesSeeder extends Seeder
 {
@@ -16,6 +17,7 @@ class SchedulesSeeder extends Seeder
     {
         DB::unprepared(file_get_contents( __dir__ . '/sql/schedules.sql'));
         DB::unprepared(file_get_contents( __dir__ . '/sql/schedule_events.sql'));
+        // Schedule::factory()->events(90)->count(10)->create(['league_size_id'=>5]);
 
     }
 }
