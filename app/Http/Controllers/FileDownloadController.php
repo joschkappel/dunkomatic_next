e53277b5c31375cb787a66c8c61b2c0c592741c5
jochen kappel <jkappel@onlinehome.de>
2022-09-13 10:48:13 +0200

    filedownload fiexd

--- app/Http/Controllers/FileDownloadController.php
+++ app/Http/Controllers/FileDownloadController.php
@@ -300,7 +300,7 @@ class FileDownloadController extends Controller
 
         Log::info('region file archive download.', ['region-id' => $region->id,'format'=>$format]);
 
-        if ($$files->count() > 0) {
+        if ($files->count() > 0) {
             $zip = new ZipArchive;
             $filename = $region->code . '-runden-reports.zip';
             Storage::disk('public')->delete($filename);
