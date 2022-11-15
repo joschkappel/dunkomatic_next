5159bfb341c17009dd169e88bc585d8605c1e063
jochen kappel <jkappel@onlinehome.de>
2022-11-15 14:18:29 +0100

    member mgmt added

--- resources/views/livewire/club/team/edit.blade.php
+++ resources/views/livewire/club/team/edit.blade.php
@@ -2,129 +2,143 @@
     @php
     $disabled = $errors->any() || empty($team_no)  || empty($training_time) || empty($training_day) || empty($preferred_game_time) || empty($shirt_color) ? true : false;
     @endphp
-
-    <x-cards.form  colWidth=6 :disabled="$disabled" cardChangeNote="{{$team_lastmod}}" cardTitle="{{ __('team.title.modify', ['team'=> $team->club['shortname'].' '.$team->team_no ]) }}" formAction="update">
-        <div class="flex flex-col m-4">
-        </div>
-
-        {{-- Team No --}}
-        <div class="flex flex-col m-4">
-            <label for='team_no' class="form-label">@lang('team.no')</label>
-            <div wire:ignore>
-                <select class="form-control select2" id="team_no">
-                @for ($i=1; $i<=9; $i++)
-                    <option @if ($i == $team->team_no) selected @endif value="{{ $i }}">{{ $i }}</option>
-                @endfor
-                </select>
-            </div>
-        </div>
-
-        <div class="flex flex-col m-4">
-            <div class="row align-items-start">
-                <div class="col-6">
-                    {{-- League --}}
-                    <label for='league' class="form-label">{{trans_choice('league.league',1)}}</label>
-                    @if ($team->league_id)
-                    <input type="text" class="form-control" readonly value="{{ $team->league['shortname'] }}"></input>
-                    @else
-                    <input type="text" class="form-control" readonly value=""></input>
-                    @endif
-                </div>
-
-                {{-- Previous League --}}
-                <div class="col-6">
-                    <label for="league_prev" class="form-label">@lang('team.league.previous')</label>
-                    <input  wire:model.debounce.500ms="league_prev" type="text" class="form-control @error('league_prev') is-invalid @else @if ($league_prev != $team->league_prev ) is-valid @endif @enderror" placeholder="@lang('team.league.previous')">
-                    @error('league_prev')
-                        <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
-            </div>
-        </div>
-
-        {{-- Tshirt Color --}}
-        <div class="flex flex-col m-4">
-            <label for="shirt_color" class="form-label">@lang('team.shirtcolor')</label>
-            <input wire:model.debounce.500ms="shirt_color" type="text" class="form-control @error('shirt_color') is-invalid @else @if ($shirt_color != $team->shirt_color ) is-valid @endif @enderror" id="shirt_color" placeholder="@lang('team.shirtcolor')"">
-            @error('shirt_color')
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
-        </div>
-
-        {{-- Training day and time --}}
-        {{-- dayOfWeek returns a number between 0 (sunday) and 6 (saturday) --}}
-        {{-- dayOfWeekIso returns a number between 1 (monday) and 7 (sunday) --}}
-        <div class="flex flex-col m-4">
-            <label for='training_day' class="form-label">@lang('team.training')</label>
-            <div class="row align-items-start">
-                <div class="col-8">
-                    <div wire:ignore>
-                        <select class="form-control select2" id="training_day">
-                            <option @if ( $training_day == '1' ) selected @endif value="1">{{ Carbon\Carbon::now()->startOfWeek(Carbon\Carbon::MONDAY)->locale(Config::get('app.locale'))->dayName }}</option>
-                            <option @if ( $training_day == '2' ) selected @endif value="2">{{ Carbon\Carbon::now()->startOfWeek(Carbon\Carbon::TUESDAY)->locale(Config::get('app.locale'))->dayName }}</option>
-                            <option @if ( $training_day == '3' ) selected @endif value="3">{{ Carbon\Carbon::now()->startOfWeek(Carbon\Carbon::WEDNESDAY)->locale(Config::get('app.locale'))->dayName }}</option>
-                            <option @if ( $training_day == '4' ) selected @endif value="4">{{ Carbon\Carbon::now()->startOfWeek(Carbon\Carbon::THURSDAY)->locale(Config::get('app.locale'))->dayName }}</option>
-                            <option @if ( $training_day == '5' ) selected @endif value="5">{{ Carbon\Carbon::now()->startOfWeek(Carbon\Carbon::FRIDAY)->locale(Config::get('app.locale'))->dayName }}</option>
-                            </option>
-                        </select>
+    <div class="container-fluid ">
+        <div class="row">
+            <div class="col-sm-6 pd-2">
+                <x-cards.form  colWidth=12 :disabled="$disabled" cardChangeNote="{{$team_lastmod}}" cardTitle="{{ __('team.title.modify', ['team'=> $team->club['shortname'].' '.$team->team_no ]) }}" formAction="update">
+                    <div class="flex flex-col m-4">
                     </div>
-                </div>
-                <div class="col-4">
-                    <div class="input-group date" id="grp_training_time" data-target-input="nearest">
-                        <input wire:model.debounce.500ms="training_time" type="text" class="form-control  datetimepicker-input  @error('training_time') is-invalid @enderror"  id="training_time"  data-target="#grp_training_time">
-                        <div  class="input-group-append" data-target="#grp_training_time" data-toggle="datetimepicker">
-                            <div class="input-group-text"><i class="far fa-clock"></i></div>
+
+                    {{-- Team No --}}
+                    <div class="flex flex-col m-4">
+                        <label for='team_no' class="form-label">@lang('team.no')</label>
+                        <div wire:ignore>
+                            <select class="form-control select2" id="team_no">
+                            @for ($i=1; $i<=9; $i++)
+                                <option @if ($i == $team->team_no) selected @endif value="{{ $i }}">{{ $i }}</option>
+                            @endfor
+                            </select>
                         </div>
-                        @error('training_time')
+                    </div>
+
+                    <div class="flex flex-col m-4">
+                        <div class="row align-items-start">
+                            <div class="col-6">
+                                {{-- League --}}
+                                <label for='league' class="form-label">{{trans_choice('league.league',1)}}</label>
+                                @if ($team->league_id)
+                                <input type="text" class="form-control" readonly value="{{ $team->league['shortname'] }}"></input>
+                                @else
+                                <input type="text" class="form-control" readonly value=""></input>
+                                @endif
+                            </div>
+
+                            {{-- Previous League --}}
+                            <div class="col-6">
+                                <label for="league_prev" class="form-label">@lang('team.league.previous')</label>
+                                <input  wire:model.debounce.500ms="league_prev" type="text" class="form-control @error('league_prev') is-invalid @else @if ($league_prev != $team->league_prev ) is-valid @endif @enderror" placeholder="@lang('team.league.previous')">
+                                @error('league_prev')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+
+                    {{-- Tshirt Color --}}
+                    <div class="flex flex-col m-4">
+                        <label for="shirt_color" class="form-label">@lang('team.shirtcolor')</label>
+                        <input wire:model.debounce.500ms="shirt_color" type="text" class="form-control @error('shirt_color') is-invalid @else @if ($shirt_color != $team->shirt_color ) is-valid @endif @enderror" id="shirt_color" placeholder="@lang('team.shirtcolor')"">
+                        @error('shirt_color')
                             <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
                     </div>
 
-                </div>
-            </div>
-        </div>
+                    {{-- Training day and time --}}
+                    {{-- dayOfWeek returns a number between 0 (sunday) and 6 (saturday) --}}
+                    {{-- dayOfWeekIso returns a number between 1 (monday) and 7 (sunday) --}}
+                    <div class="flex flex-col m-4">
+                        <label for='training_day' class="form-label">@lang('team.training')</label>
+                        <div class="row align-items-start">
+                            <div class="col-8">
+                                <div wire:ignore>
+                                    <select class="form-control select2" id="training_day">
+                                        <option @if ( $training_day == '1' ) selected @endif value="1">{{ Carbon\Carbon::now()->startOfWeek(Carbon\Carbon::MONDAY)->locale(Config::get('app.locale'))->dayName }}</option>
+                                        <option @if ( $training_day == '2' ) selected @endif value="2">{{ Carbon\Carbon::now()->startOfWeek(Carbon\Carbon::TUESDAY)->locale(Config::get('app.locale'))->dayName }}</option>
+                                        <option @if ( $training_day == '3' ) selected @endif value="3">{{ Carbon\Carbon::now()->startOfWeek(Carbon\Carbon::WEDNESDAY)->locale(Config::get('app.locale'))->dayName }}</option>
+                                        <option @if ( $training_day == '4' ) selected @endif value="4">{{ Carbon\Carbon::now()->startOfWeek(Carbon\Carbon::THURSDAY)->locale(Config::get('app.locale'))->dayName }}</option>
+                                        <option @if ( $training_day == '5' ) selected @endif value="5">{{ Carbon\Carbon::now()->startOfWeek(Carbon\Carbon::FRIDAY)->locale(Config::get('app.locale'))->dayName }}</option>
+                                        </option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="input-group date" id="grp_training_time" data-target-input="nearest">
+                                    <input wire:model.debounce.500ms="training_time" type="text" class="form-control  datetimepicker-input  @error('training_time') is-invalid @enderror"  id="training_time"  data-target="#grp_training_time">
+                                    <div  class="input-group-append" data-target="#grp_training_time" data-toggle="datetimepicker">
+                                        <div class="input-group-text"><i class="far fa-clock"></i></div>
+                                    </div>
+                                    @error('training_time')
+                                        <div class="invalid-feedback">{{ $message }}</div>
+                                    @enderror
+                                </div>
 
-        {{-- Preferred game day and time --}}
-        <div class="flex flex-col m-4">
-            <label for='preferred_game_day' class="form-label">@lang('team.game.preferred')</label>
-            <div class="row align-items-start">
-                <div class="col-8">
-                    <div wire:ignore>
-                        <select  class="form-control select2" id="preferred_game_day">
-                            <option @if ($preferred_game_day == '6' ) selected @endif value="6">{{ Carbon\Carbon::now()->startOfWeek(Carbon\Carbon::SATURDAY)->locale(Config::get('app.locale'))->dayName }}</option>
-                            <option @if ($preferred_game_day == '7' ) selected @endif value="7">{{ Carbon\Carbon::now()->startOfWeek(Carbon\Carbon::SUNDAY)->locale(Config::get('app.locale'))->dayName }}</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="col-4">
-                    <div class="input-group date" id="grp_preferred_game_time" data-target-input="nearest">
-                        <input wire:model.debounce.500ms="preferred_game_time" type="text" class="form-control datetimepicker-input @error('preferred_game_time') is-invalid @enderror" data-target="#grp_preferred_game_time" id="preferred_game_time"/>
-                        <div class="input-group-append" data-target="#grp_preferred_game_time" data-toggle="datetimepicker">
-                            <div class="input-group-text"><i class="far fa-clock"></i></div>
+                            </div>
                         </div>
-                        @error('preferred_game_time')
-                        <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
                     </div>
-                </div>
+
+                    {{-- Preferred game day and time --}}
+                    <div class="flex flex-col m-4">
+                        <label for='preferred_game_day' class="form-label">@lang('team.game.preferred')</label>
+                        <div class="row align-items-start">
+                            <div class="col-8">
+                                <div wire:ignore>
+                                    <select  class="form-control select2" id="preferred_game_day">
+                                        <option @if ($preferred_game_day == '6' ) selected @endif value="6">{{ Carbon\Carbon::now()->startOfWeek(Carbon\Carbon::SATURDAY)->locale(Config::get('app.locale'))->dayName }}</option>
+                                        <option @if ($preferred_game_day == '7' ) selected @endif value="7">{{ Carbon\Carbon::now()->startOfWeek(Carbon\Carbon::SUNDAY)->locale(Config::get('app.locale'))->dayName }}</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="input-group date" id="grp_preferred_game_time" data-target-input="nearest">
+                                    <input wire:model.debounce.500ms="preferred_game_time" type="text" class="form-control datetimepicker-input @error('preferred_game_time') is-invalid @enderror" data-target="#grp_preferred_game_time" id="preferred_game_time"/>
+                                    <div class="input-group-append" data-target="#grp_preferred_game_time" data-toggle="datetimepicker">
+                                        <div class="input-group-text"><i class="far fa-clock"></i></div>
+                                    </div>
+                                    @error('preferred_game_time')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {{-- Preferred gym --}}
+                    <div wire:ignore class="flex flex-col m-4">
+                        <label for='gym_id' class="form-label">{{ __('team.gym.preferred') }}</label>
+                        <div wire:ignore> {{-- [tl! highlight] --}}
+                            <div class="input-group">
+                                <select  class="form-control select2" id="gym_id">
+                                    @foreach ($team->club->gyms as $g )
+                                    <option value="{{ $g->id }}">{{ $g->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+                </x-cards.form>
+            </div>
+            <div class="col-sm-6">
+                <!-- card MEMBERS -->
+                <x-member-card :members="$members" :entity="$team" entity-class="App\Models\Team" />
+                <!-- /.card -->
             </div>
         </div>
+    </div>
+    <!-- all modals here -->
+    <x-confirm-deletion modalId="modalDeleteMember" modalTitle="{{ __('role.title.delete') }}" modalConfirm="{{ __('role.confirm.delete') }}" deleteType="{{ trans_choice('role.member', 1) }}" />
 
-        {{-- Preferred gym --}}
-        <div wire:ignore class="flex flex-col m-4">
-            <label for='gym_id' class="form-label">{{ __('team.gym.preferred') }}</label>
-            <div wire:ignore> {{-- [tl! highlight] --}}
-                <div class="input-group">
-                    <select  class="form-control select2" id="gym_id">
-                        @foreach ($team->club->gyms as $g )
-                        <option value="{{ $g->id }}">{{ $g->name }}</option>
-                        @endforeach
-                    </select>
-                </div>
-            </div>
-        </div>
-
-    </x-cards.form>
+    <!-- all modals above -->
 </div>
 
 @push('js')
@@ -193,6 +207,14 @@
         $('#gym_id').on('change', function (e) {
             var data = $('#gym_id').select2("val");
             @this.set('gym_id', data);
+        });
+        $("button#deleteMember").click(function() {
+            $('#modalDeleteMember_Instance').html($(this).data('member-name'));
+            var url =
+                "{{ route('membership.team.destroy', ['team' => $team, 'member' => ':member:']) }}";
+            url = url.replace(':member:', $(this).data('member-id'));
+            $('#modalDeleteMember_Form').attr('action', url);
+            $('#modalDeleteMember').modal('show');
         });
     });
   </script>
