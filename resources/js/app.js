2c42d2faa40bc82da1d001ccd1b0b845541db925
jochen kappel <jkappel@onlinehome.de>
2024-07-09 17:26:48 +0200

    GUI working ok

--- resources/js/app.js
+++ resources/js/app.js
@@ -31,10 +31,11 @@ try {
 
     // bootstrap datatables...
     require( 'jszip' );
+    require('jquery');
     require( 'datatables.net-bs4' );
     require( 'datatables.net-buttons-bs4' );
     require( 'datatables.net-buttons/js/buttons.colVis.js' );
-    require( 'datatables.net-buttons/js/buttons.flash.js' );
+    // require( 'datatables.net-buttons/js/buttons.flash.js' );
     require( 'datatables.net-buttons/js/buttons.html5.js' );
     require( 'datatables.net-buttons/js/buttons.print.js' );
     require( 'datatables.net-colreorder-bs4' );
